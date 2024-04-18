cfa8c29a6952b1588d1b3b630f0e84c260513c25
prtybuss <prtybuss@gmail.com>
2024-04-18 04:31:25 +0300

    Changes to be committed: modified:   .env.sample modified:   backend/server.js modified:   docker-compose.yml Changes not staged for commit: modified:   .gitignore modified:   backend/models/offices.js

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,16 @@
 
 const mongoose = require('mongoose');
 const app = require('./app');
-const {
-	DB_USER,
-	DB_PASSWORD,
-	DB_HOST,
-	DB_PORT,
-	DB_NAME,
-} = process.env;
+
 /* const dotenv = require('dotenv');
 dotenv.config({
 	path: './config.env'
-}); */
+}); 
 var corsOptions = {
 	origin: process.env.CLIENT_ORIGIN || "http://localhost:8081"
 };
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions));*/
 
 
 
@@ -25,11 +19,10 @@ app.use(cors(corsOptions));
 const start = async () => {
 	console.log('mode:', process.env.NODE_ENV);
 	try {
-		await mongoose.connect(/* process.env.DB */`mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`);
-		console.log('db connected');
-		//const PORT = process.env.NODE_DOCKER_PORT || 8080;
-		app.listen(/* process.env.PORT */(process.env.NODE_DOCKER_PORT || 8080), () => {
-			console.log("server started on", process.env.PORT, 'port');
+		await mongoose.connect(require('./config/db.config'));
+
+		app.listen(process.env.NODE_DOCKER_PORT, () => {
+			console.log("server started on", process.env.NODE_DOCKER_PORT, 'port');
 		})
 	}
 	catch (error) {
